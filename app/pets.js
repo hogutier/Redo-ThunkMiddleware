e42f5af49c166d46fed5795d4d71665a67b09644
Thomas Kelly <tmkelly28@gmail.com>
2018-03-24 16:41:48 -0400

    setup

--- app/pets.js
+++ app/pets.js
@@ -2,23 +2,25 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {List} from './utils'
 
-const Pets = (props) => {
-  const {pets} = props
+class Pets extends React.Component {
+  render () {
+    const {pets} = this.props
 
-  return (
-    <div>
-      <List
-        forEachOfThese={pets}
-        doThis={pet => (
-          <div key={pet.id}>
-            <img src={pet.imageUrl} />
-            <p>{pet.name}</p>
-          </div>
-        )}
-        unlessEmpty={() => <div>Where are the pets?!?</div>}
-      />
-    </div>
-  )
+    return (
+      <div>
+        <List
+          forEachOfThese={pets}
+          doThis={pet => (
+            <div key={pet.id}>
+              <img src={pet.imageUrl} />
+              <p>{pet.name}</p>
+            </div>
+          )}
+          unlessEmpty={() => <div>Where are the pets?!?</div>}
+        />
+      </div>
+    )
+  }
 }
 
 const mapState = (state) => {
